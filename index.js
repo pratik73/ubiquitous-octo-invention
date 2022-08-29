680bff147bfaa315171f5ed3b41550cd8ea53ba9
Pratik <pratik1070061@gmail.com>
2022-08-28 20:41:35 -0400

    Client code: Add a file in root of the project and run it

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const express = require("express");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-
 const app = express();
+const upload = require("express-fileupload");
+var axios = require("axios");
+var FormData = require("form-data");
+var fs = require("fs");
 
-app.post("/profile", upload.single("avatar"), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-});
+app.use(upload());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.post(
-  "/photos/upload",
-  upload.array("photos", 12),
-  function (req, res, next) {
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
-  }
-);
-
-const cpUpload = upload.fields([
-  { name: "avatar", maxCount: 1 },
-  { name: "gallery", maxCount: 8 },
-]);
-app.post("/cool-profile", cpUpload, function (req, res, next) {
-  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-  //
-  // e.g.
-  //  req.files['avatar'][0] -> File
-  //  req.files['gallery'] -> Array
-  //
-  // req.body will contain the text fields, if there were any
-});
-const port = process.env.PORT || 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello People");
+  putToTargetServer(res);
+
+  // res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log("Server is up on port " + port);
+  console.log(`Example app listening on port ${port}`);
 });
+
+const putToTargetServer = (res) => {
+  var data = new FormData();
+  data.append("temp", fs.createReadStream("./DJI_0528.MP4"));
+
+  var config = {
+    method: "put",
+    url: "http://localhost:3000",
+    headers: {
+      ...data.getHeaders(),
+    },
+    data: data,
+    maxBodyLength: Infinity,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.send("Upload Successful");
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send("Upload Failed");
+    });
+};
